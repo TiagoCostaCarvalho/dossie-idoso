14eec8b17e560ed08ff297e769d4513b3f56fa3f
maplepls <64789755+maplepls@users.noreply.github.com>
2024-05-23 17:02:05 -0300

    feat: adding libras and mui

---
+++ src/components/Test/Test.js
@@ -0,0 +1,13 @@
+import './Test.css';
+import Button from '@mui/material/Button';
+
+function Test() {
+  return (
+    <div className="App">
+      <Button variant="contained">Hello world</Button>;
+      Texto teste brabo.
+    </div>
+  );
+}
+
+export default Test;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Test from './components/Test/Test';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+      />
+
+
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+
+    </head>
+    {/* <App /> */}
+    <Test />
   </React.StrictMode>
 );
 
