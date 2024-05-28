70b4302aa97a76aa210b04153f6467ea3d83292b
Paul Dany Flores Atauchi <FloresAtauchi@gmail.com>
2024-05-28 12:18:19 -0300

    Add Personalized Light Theme (#4)
    * style: add empty light theme file
    
    * added the palette of light theme
    
    ---------
    
    Co-authored-by: YuviQP <yuvisa.palomino@gmail.com>

---
+++ src/views/themes/dossieIdosoLightTheme.js
@@ -0,0 +1,31 @@
+import { createTheme } from "@mui/material/styles";
+
+const dossieIdosoLightTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#6750A4",
+
+    },
+    secondary: {
+      main: "#625B71",
+    
+    },
+    tertiary: {
+      main: "#7D5260",
+    },
+    error: {
+      main: "#B3261E",
+    },
+    background: {
+      default: "#ffffff",
+    },
+    typography: {
+        fontFamily: "Roboto, Arial, sans-serif",
+      },
+  },
+
+  
+});
+
+export default dossieIdosoLightTheme;
