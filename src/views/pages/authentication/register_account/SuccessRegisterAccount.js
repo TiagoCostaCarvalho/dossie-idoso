0fd83dd895718e5d17c4942b5c79f9896860b895
YuviQP <yuvisa.palomino@gmail.com>
2024-05-28 23:04:26 -0300

    added the styles of signup page

--- src/views/pages/authentication/register_account/SuccessRegisterAccount.js
+++ src/views/pages/authentication/register_account/SuccessRegisterAccount.js
@@ -1,33 +1,56 @@
 import { useNavigate } from "react-router-dom";
-import { Button } from "@mui/material";
+import { Button, Card, Container, Typography, Stack } from "@mui/material";
 
 function SuccessRegisterAccount() {
-    const user = localStorage.getItem("user");
-    const navigate = useNavigate();
+  const user = localStorage.getItem("user");
+  const navigate = useNavigate();
 
-    return (
-        <div>
-            <p>Bem vindo,</p>
-            <p>{user}!</p>
+  return (
+    <>
+      <Container sx={{ marginBottom: 8, padding: 15 }}>
+        <Typography variant="h6" align="center">
+          Bem vindo,
+        </Typography>
+        <Typography variant="h6" align="center">
+          {user}!
+        </Typography>
 
-            <p>Conta criada com sucesso!</p>
+        <Typography variant="h6" align="center">
+          Conta criada com sucesso!
+        </Typography>
 
-            <div>
-                <p>Agora você pode criar e editar conteúdo para o Dossiê Idoso!</p>
+        <Card
+          sx={{
+            backgroundColor: "#F7F2FA",
+            padding: 25,
+            borderRadius: 2,
+            width: "100%",
+            textAlign: "center",
+            alignContent: "center",
+            flexDirection: "column",
+            variant: "outlined",
+          }}
+        >
+          <Typography variant="h6" align="center">
+            Agora você pode criar e editar conteúdo para o Dossiê Idoso!
+          </Typography>
+          <Stack spacing={2} direction="column">
+            <Button variant="contained" onClick={(e) => navigate("/new-topic")}>
+              Começar novo conteúdo!
+            </Button>
 
-                <Button
-                    variant="contained"
-                    onClick={(e) => navigate('/new-topic')}
-                >Começar novo conteúdo!</Button>
-
-                <Button
-                    color="secondary"
-                    variant="contained"
-                    onClick={(e) => navigate('/new-topic/example')}
-                >Como criar conteúdo?</Button>
-            </div>
-        </div>
-    );
-};
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={(e) => navigate("/new-topic/example")}
+            >
+              Como criar conteúdo?
+            </Button>
+          </Stack>
+        </Card>
+      </Container>
+    </>
+  );
+}
 
 export default SuccessRegisterAccount;
