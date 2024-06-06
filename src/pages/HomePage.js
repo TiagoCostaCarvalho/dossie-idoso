2505d5768d60b20f609a5d6ced2a0faa8a14af2c
Maurício Catellan <mauriciocatellan@gmail.com>
2024-06-05 23:35:23 -0300

    Exemplos reais de tópicos

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,66 +1,79 @@
 import Test from '../components/Test/Test';
 import BaseContainer from '../components/BaseContainer/BaseContainer';
 import MainHeader from '../components/MainHeader/MainHeader';
-import imageUrl from "../resources/foca-harpa.jpg"
+import idosoCorridaUrl from "../resources/idosoCorrida.jpg";
+import idosoDiabetesUrl from "../resources/idosoDiabetes.jpg";
+import idosoTristeUrl from "../resources/idosoTriste.jpg";
+
+import idosoFestaUrl from "../resources/idosoFesta.jpeg";
+import idosoJogoUrl from "../resources/idosoJogo.jpg";
+import idosoBoardgameUrl from "../resources/idosoBoardgame.jpg";
+
+import dinheirosAntigosUrl from "../resources/dinheirosAntigos.jpg";
+import coisasAntigasUrl from "../resources/coisasAntigas.jpg";
+import idosoCelular from "../resources/idosoCelular.jpg";
 
 function HomePage(props) {
   const saudeCards = [{
-    title: "Titulo muito louco",
-    description: "Descricao muito louca",
-    //imgUrl: "https://escolakids.uol.com.br/upload/conteudo/images/2018/12/foca-harpa.jpg",
-    imgUrl: imageUrl,
-    imgAlt: "Foca harpa"
+    title: "Exercite-se, idoso!",
+    description: "Veja dicas de exercícios para fazer!", 
+    imgUrl: idosoCorridaUrl,
+    imgAlt: "Idosos correndo"
   },
   {
-    title: "Aawawa",
-    description: "Aawaaawawa"
+    title: "Como conviver com a diabetes?",
+    description: "Seja um idoso diabético saudável!",
+    imgUrl: idosoDiabetesUrl,
+    imgAlt: "Idoso medindo sua diabetes"
   },
   {
-    title: "BBBBB",
-    description: "BBBBBB"
-  },
-  {
-    title: "CCCCC",
-    description: "CCCCC"
-  },
-  {
-    title: "DDDDD",
-    description: "DDDDD"
-  },
-  {
-    title: "EEEEE",
-    description: "EEEEE"
-  }
+    title: "Depressão em idosos",
+    description: "Veja dicas de como tratar e prevenir.",
+    imgUrl: idosoTristeUrl,
+    imgAlt: "Idoso triste"
+  }  
 ]
 
 const lazerCards = [{
-  title: "Lazer muito louco",
-  description: "Descricao muito louca"
+  title: "Como encontrar festas para idosos",
+  description: "Busque festas na sua região.",
+  imgUrl: idosoFestaUrl,
+  imgAlt: "Idosos festejando"
 },
 {
-  title: "Lazer muito louco",
-  description: "Descricao muito louca"
+  title: "Melhores videogames para a 3ª idade",
+  description: "Jogos com muita acessibilidade.",
+  imgUrl: idosoJogoUrl,
+  imgAlt: "Idoso jogando videogame"
 },
 {
-  title: "Lazer muito louco",
-  description: "Descricao muito louca"
+  title: "Dicas de boardgames para jogar com o neto",
+  description: "Diversão à beça para a família.",
+  imgUrl: idosoBoardgameUrl,
+  imgAlt: "Idoso jogando boardgame com criança"
 }
 ]
 
 const economiaCards = [{
-  title: "Economia muito louca",
-  description: "Descricao muito louca"
+  title: "O que fazer com cédulas antigas?",
+  description: "Dicas de como conservar ou vender.",
+  imgUrl: dinheirosAntigosUrl,
+  imgAlt: "Cédulas e moedas antigas"
 },
 {
-  title: "Economia Aawawa",
-  description: "Aawaaawawa"
+  title: "Como vender objetos inutilizados",
+  description: "Tire a poeira do seu sótão!",
+  imgUrl: coisasAntigasUrl,
+  imgAlt: "Diversos objetos empoeirados"
 },
 {
-  title: "Economia BBBBB",
-  description: "BBBBBB"
+  title: "Melhores bancos digitais para idosos",
+  description: "Bancos digitais com boa acessibilidade.",
+  imgUrl: idosoCelular,
+  imgAlt: "Idoso utilizando um celular"
 }
 ]
-
+ 
     return (
       <>
         <MainHeader/>
