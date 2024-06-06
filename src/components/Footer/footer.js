c2a77958bcadb8d84e97cf06cbbb93c35453c5f1
Valsirene <valsirenesp@gmail.com>
2024-06-06 09:34:07 -0300

    update footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,23 +1,49 @@
 import { Link } from "@mui/material";
 import "components/Footer/Footer.css";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import XIcon from "@mui/icons-material/X";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 function Footer() {
   return (
-    <footer>
-      <div className="footer-top">
-        
-                <Link>Sobre</Link>
-            
-                <Link>Termos de uso</Link>
-          
-                <Link>Perguntas Frequentes</Link>
-           
-                <Link>Políticas de Privacidade</Link>
-          
-        </div>
-        <div className="footer">
-         <span>Todos direitos reservados</span> &copy; 2024
-        </div>
+    <footer className="footer-top">
+      <ul>
+        <li>
+          <Link className="sobre" to="/" href="#" underline="always">
+            {"Sobre"}
+          </Link>
+        </li>
+        <li>
+          <Link className="perguntas-frequentes" href="#" underline="always">
+            {"Perguntas Frequentes"}
+          </Link>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <Link className="termos-uso" href="#" underline="always">
+            {"Termos de uso"}
+          </Link>
+        </li>
+        <li>
+          <Link className="politica-privacidade" href="#" underline="always">
+            {"Políticas de Privacidade"}
+          </Link>
+        </li>
+      </ul>
+      
+      <span>
+        <FacebookIcon />
+        <WhatsAppIcon />
+        <XIcon />
+        <InstagramIcon />
+      </span>
+
+      <span className="footer-botton">
+        &copy; 2024 DossiêIdoso, Todos direitos reservados
+      </span>
     </footer>
   );
 }
