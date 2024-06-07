de771a57ea2b63b782f31bc14e7d3f649320fd19
patauchi <floresatauchi@gmail.com>
2024-06-06 23:03:52 -0300

    style: responsivility in pages login and register account

--- src/views/pages/authentication/register_account/SuccessRegisterAccount.js
+++ src/views/pages/authentication/register_account/SuccessRegisterAccount.js
@@ -1,16 +1,16 @@
 import { useNavigate } from "react-router-dom";
-import { Button, Card, Container, Typography, Stack, Link, Breadcrumbs } from "@mui/material";
+import { Button, Card, Container, Typography, Stack, Link, Breadcrumbs, Grid } from "@mui/material";
 import BreadcrumbsContainer from 'components/BreadcrumbsContainer/BreadcrumbsContainer';
 import MainHeader from "components/MainHeader/MainHeader";
 import Footer from "components/Footer/footer";
 
 function SuccessRegisterAccount() {
-  const user = localStorage.getItem("user");
+  const user = JSON.parse(localStorage.getItem("user"));
   const navigate = useNavigate();
 
   return (
     <>
-      <MainHeader/>
+      <MainHeader />
       <BreadcrumbsContainer>
         <Breadcrumbs aria-label="breadcrumb">
           <Link underline="hover" color="#21005D" href="/">
@@ -19,7 +19,7 @@ function SuccessRegisterAccount() {
           <Typography color="#21005D">Criar Conta</Typography>
         </Breadcrumbs>
       </BreadcrumbsContainer>
-      <Container sx={{ marginBottom: 8, padding: 15 }}>
+      <Container>
         <Typography variant="h6" align="center">
           Bem vindo,
         </Typography>
@@ -27,41 +27,57 @@ function SuccessRegisterAccount() {
           {user.name}!
         </Typography>
 
-        <Typography variant="h6" align="center">
+        <Typography variant="h6" align="center" sx={{ "mt": "2em" }}>
           Conta criada com sucesso!
         </Typography>
+
 
         <Card
           sx={{
             backgroundColor: "#F7F2FA",
-            padding: 25,
+            margin: "1em",
+            padding: "1em",
             borderRadius: 2,
-            width: "100%",
+            // width: "100%",
             textAlign: "center",
             alignContent: "center",
             flexDirection: "column",
             variant: "outlined",
           }}
         >
-          <Typography variant="h6" align="center">
-            Agora você pode criar e editar conteúdo para o Dossiê Idoso!
-          </Typography>
-          <Stack spacing={2} direction="column">
-            <Button variant="contained" onClick={(e) => navigate("/new-topic")}>
-              Começar novo conteúdo!
-            </Button>
 
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={(e) => navigate("/new-topic/example")}
-            >
-              Como criar conteúdo?
-            </Button>
-          </Stack>
+
+          <Grid container spacing={2} sx={{
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+            <Grid item xs={12} md={8} lg={8} textAlign={"center"}>
+
+              <Typography variant="h6" align="center">
+                Agora você pode criar e editar conteúdo para o Dossiê Idoso!
+              </Typography>
+            </Grid>
+
+            <Grid item xs={12} md={8} lg={8} textAlign={"center"}>
+              <Stack spacing={2} direction="column">
+                <Button variant="contained" onClick={(e) => navigate("/new-topic")}>
+                  Começar novo conteúdo!
+                </Button>
+
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={(e) => navigate("/new-topic/example")}
+                >
+                  Como criar conteúdo?
+                </Button>
+              </Stack>
+            </Grid>
+          </Grid>
         </Card>
+
       </Container>
-      <Footer/>
+      <Footer />
     </>
   );
 }
